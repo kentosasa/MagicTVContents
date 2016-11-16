cf651b40d5baac6e711c618f4d7a4ff7f6143fbe
kento sasamoto <knote0721@gmail.com>
2016-11-16 17:54:27 +0900

    contents

---
+++ Hue/src/LightOnYellow.js
@@ -0,0 +1,34 @@
+var request = require('superagent')
+var IPURL = 'https://www.meethue.com/api/nupnp';
+var hueapi = ''; //
+var USERNAME = '1234567890';
+var flag = true;
+
+
+function createEndpoint(ip){
+  return 'http://'+ip+'/api/'+USERNAME+'/lights/';
+}
+
+request
+  .get(IPURL)
+  .set('Accept', 'application/json')
+  .end(function(err, res){
+    var ipadress = res.body[0].internalipaddress; //HueのIPアドレス
+    hueapi = createEndpoint(ipadress);
+  });
+
+function ctrlHue(id, flag){
+  if(hueapi === '')return;
+
+  request
+    .put(hueapi+id+'/state')
+    .send({hue: 12750})
+    .set('Accept', 'application/json')
+    .end(function(err, res){
+      console.log(res.body[0]);
+    });
+}
+
+setInterval(function(){
+  ctrlHue(1, flag);
+},1000);
---
+++ addImage.rb
@@ -0,0 +1,43 @@
+require 'json'
+require 'pry'
+
+def add_type(json)
+  if json["children"]
+    json["type"] = "menu"
+  else
+    json["type"] = "web"
+  end
+  json["image"] = ARGV[1]
+  return json
+end
+
+def has_children(jsons)
+  return unless jsons["children"]
+  jsons["children"].each do |json|
+    add_type(json)
+    has_children(json)
+  end
+  jsons["children"]
+end
+
+json_file_path = ARGV[0]
+
+# 読み込んで
+json_data = open(json_file_path) do |io|
+  JSON.load(io)
+end
+
+jsons = json_data["children"]
+
+# 更新して
+jsons.each do |json|
+  add_type(json)
+  has_children(json)
+end
+
+json_data["children"] = jsons
+
+# 保存する
+open(json_file_path, 'w') do |io|
+  JSON.dump(json_data, io)
+end
