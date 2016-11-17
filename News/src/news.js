31d1d006e816a29702558c852687b71dd333ad89
kento sasamoto <knote0721@gmail.com>
2016-11-17 18:00:29 +0900

    read news

--- News/src/news.js
+++ News/src/news.js
@@ -3,12 +3,27 @@ let count = 0
 for(let entry of entries) {
   $("#contents").append("<div id='" + count + "' class='item'><div class='card card-block'>"+
     "<h4 class='card-title'>" + entry["title"] + "</h4>" +
-    "<p class='card-text'>" + entry["text"] + "</p>" +
+    "<p class='card-text'>" + entry["text"].replace(/\n/g,'<br>') + "</p>" +
     "</div></div class='item'>"
     );
   count++
 }
 count = 0
+
+let synthesis = new SpeechSynthesisUtterance()
+synthesis.lang = 'ja-JP'
+synthesis.rate = 1.0
+
+synthesis.onend = () => {
+  slide()
+  play(entries[count])
+}
+
+let play = (entry) => {
+  synthesis.text = entry["title"] + "\n\n" + entry["text"]
+  speechSynthesis.speak(synthesis)
+}
+
 let slide = function() {
   if (count == entries.length-1) {
     $(".item").removeClass("hide")
@@ -18,4 +33,4 @@ let slide = function() {
   $(id).addClass("hide")
   count++
 }
-setInterval(slide, 20000)
+play(entries[0])
